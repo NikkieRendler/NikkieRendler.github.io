b41836e845c490fb1d421c2c68c2de151c66efe6
NikkieRendler <38360153+NikkieRendler@users.noreply.github.com>
2018-05-25 17:38:18 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
+$("input").on("keyup", function(e) {
+  if(e.keyCode === 13) {
+  $(".fa-search").trigger("click");
+}});
 $(".fa-search").on("click", function() {
+
+  $(".info span").remove();
+  $(".temp span").remove();
+  $(".appRightSide div").remove();
+  $(".weatherRight div").remove();
+  $(".weatherRight").append("<div class='loader'></div>");
   var requiredCity = $("input").val();
 
 $.get(`https://api.wunderground.com/api/bc83691ee725e7bb/geolookup/conditions/hourly/q/UA/${requiredCity}.json`, function(parsed_json) {
@@ -8,10 +18,7 @@ $.get(`https://api.wunderground.com/api/bc83691ee725e7bb/geolookup/conditions/ho
   var wind = parsed_json.current_observation.wind_kph;
   var sky = parsed_json.current_observation.weather;
   var skyIcon = parsed_json.current_observation.icon_url;
-  $(".info span").remove();
-  $(".temp span").remove();
-  $(".wearWhat div").remove();
-  $(".weatherRight div").remove();
+
   console.log(parsed_json);
 
   $(".weatherBgr").css("backgroundImage", "url(img/cityBgr.png)");
@@ -34,7 +41,7 @@ $.get(`https://api.wunderground.com/api/bc83691ee725e7bb/geolookup/conditions/ho
     $(".searchBlock").css("background", "rgba(38, 166, 227, 0.9)");
     $("footer").css("background", "rgba(38, 166, 227, 0.9)");
     $(".weather").css({"background": "rgba(38, 166, 227, 0.9)", "box-shadow": "0px 0px 10px #000"})
-    $(".wearWhat").append(`
+    $(".appRightSide").append(`
     <div><img src='img/boyCold.png' height= 200px width=200px></div>
     <div><img src='img/girlCold.png' height= 200px width=200px></div>
     `);
@@ -44,18 +51,18 @@ $.get(`https://api.wunderground.com/api/bc83691ee725e7bb/geolookup/conditions/ho
     $(".searchBlock").css("background", "rgba(134, 46, 227, 0.9)");
     $(".weather").css({"background": "rgba(134, 46, 227, 0.9)", "box-shadow": "0px 0px 10px #000"})
     $("footer").css("background", "rgba(134, 46, 227, 0.9)");
-    $(".wearWhat").append(`
+    $(".appRightSide").append(`
     <div><img src='img/boyCool.png' height= 200px width=200px></div>
     <div><img src='img/girlCool.png' height= 200px width=200px></div>
     `);
   } else if(temp_c <18) {
 
-    $("#body").css("background",  "rgba(234, 193, 46, 0.1)")
-    $("header").css("background", "rgba(234, 193, 46, 0.9)");
+    $("#body").css({"background":  "rgba(234, 193, 46, 0.1)"})
+    $("header").css({"background": "rgba(234, 193, 46, 0.9)", "box-shadow": "0px 0px 10px"});
     $(".searchBlock").css("background", "rgba(234, 193, 46, 0.9)")
     $(".weather").css({"background": "rgba(234, 193, 46, 0.9)", "box-shadow": "0px 0px 10px #000"});
-    $("footer").css("background", "rgba(234, 193, 46, 0.9)");
-    $(".wearWhat").append(`
+    $("footer").css({"background": "rgba(234, 193, 46, 0.9)", "box-shadow": "0px 0px 10px #000"});
+    $(".appRightSide").append(`
     <div><img src='img/boyWarm.png' height= 200px width=200px></div>
     <div><img src='img/girlWarm.png' height= 200px width=200px></div>
     `);
@@ -66,13 +73,16 @@ $.get(`https://api.wunderground.com/api/bc83691ee725e7bb/geolookup/conditions/ho
     $(".searchBlock").css("background", "rgba(176, 50, 18, 0.9)");
     $(".weather").css({"background":  "rgba(176, 50, 18, 0.9)", "box-shadow": "0px 0px 10px #000"});
     $("footer").css("background", "rgba(176, 50, 18, 0.9)");
-    $(".wearWhat").append(`
+    $(".appRightSide").append(`
     <div><img src='img/boyHot.png' height= 200px width=200px></div>
     <div><img src='img/girlHot.png' height= 200px width=200px></div>
     `); 
- } else {
+  } else {
   document.html("It's really cold or really hot. Take a look outside and choose obvious things")
- };
+  };
+  $(document).ready(function() {
+    $("div.loader").remove();     
+ });
   });
   });
   $(".fa-search").trigger("click");
